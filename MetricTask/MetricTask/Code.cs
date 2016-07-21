bfedd20364653e5df203f4bcebe8252ed303b821
laerteneto <laerte-m@hotmail.com>
2016-07-21 12:05:21 -0400

    New file generator
    Here, we have the new version with database integration of our file
    generator

---
+++ MetricTask/MetricTask/Code.cs
@@ -0,0 +1,49 @@
+﻿using MetricTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetricTask
+{
+    public class Code
+    {
+        public MetricTask.Models.Task[] getTasks()
+        {
+            //Code You need
+            TestBedContext db = new TestBedContext();
+            IQueryable<MetricTask.Models.Task> tasks = from t in db.Tasks
+                                                      select t;
+
+            return tasks.ToArray();
+        }
+
+        public MetricTask.Models.Metric[] getMetrics()
+        {
+            //Code You need
+            TestBedContext db = new TestBedContext();
+            IQueryable<MetricTask.Models.Metric> metrics = from m in db.Metrics
+                                                       select m;
+
+            return metrics.ToArray();
+        }
+
+        public List<Task_Metric> weightsForTask(String task_id)
+        {
+            MetricTask.Models.Task[] tasks = this.getTasks();
+            List<MetricTask.Models.Task_Metric> taskmetrics = new List<Task_Metric>();
+
+            foreach (MetricTask.Models.Task task in tasks)
+            {
+                foreach (Task_Metric metric in task.Task_Metric)
+                {
+                    taskmetrics.Add(metric);
+                }
+            }
+
+            return taskmetrics;
+        }
+
+    }
+}
---
+++ MetricTask/MetricTask/Models/Mapping/Task_MetricMap.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace MetricTask.Models.Mapping
+{
+    public class Task_MetricMap : EntityTypeConfiguration<Task_Metric>
+    {
+        public Task_MetricMap()
+        {
+            // Primary Key
+            this.HasKey(t => new { t.Task_ID, t.Metric_Name });
+
+            // Properties
+            this.Property(t => t.Metric_Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            // Table & Column Mappings
+            this.ToTable("Task_Metric");
+            this.Property(t => t.Task_ID).HasColumnName("Task_ID");
+            this.Property(t => t.Metric_Name).HasColumnName("Metric_Name");
+            this.Property(t => t.Metric_Weight).HasColumnName("Metric_Weight");
+            this.Property(t => t.Metric_Value).HasColumnName("Metric_Value");
+
+            // Relationships
+            this.HasRequired(t => t.Metric)
+                .WithMany(t => t.Task_Metric)
+                .HasForeignKey(d => d.Metric_Name);
+            this.HasRequired(t => t.Task)
+                .WithMany(t => t.Task_Metric)
+                .HasForeignKey(d => d.Task_ID);
+
+        }
+    }
+}
