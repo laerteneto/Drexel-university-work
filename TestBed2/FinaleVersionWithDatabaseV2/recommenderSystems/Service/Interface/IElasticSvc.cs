400a4aefa3d838030723622aadd6069dad11dde0
larissacfs <lcfs@cin.ufpe.br>
2016-07-25 13:15:23 -0400

    New version Testbed
    In this new version, job difficulty and self rating are not important.
    The similarity is calculated using a new Manhattan Distance function.

---
+++ TestBed2/FinaleVersionWithDatabaseV2/recommenderSystems/Service/Interface/IElasticSvc.cs
@@ -0,0 +1,17 @@
+﻿using recommenderSystems.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recommenderSystems.Service.Interface
+{
+    public interface IElasticSvc : IService
+    {
+        bool insertRecommenderJob(DataResult avgs);
+        bool insertRatings(String[] expressions, UserProfile[] users, double[,] Y);
+        int[,] getYIndex(String jobID, String recruiteeID, String[] expressions, UserProfile[] users);
+        double[,] selectRatings(String[] expressions, UserProfile[] users);
+    }
+}
---
+++ TestBed2/FinaleVersionWithDatabaseV2/recommenderSystems/Service/Plugin/JobSvcImpl.cs
@@ -0,0 +1,41 @@
+﻿using recommenderSystems.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recommenderSystems.Service.Plugin
+{
+    public class JobSvcImpl : IJobSvc
+    {
+        //jobs ids (used in expressions file)
+        public string[] selectExpressionNames()
+        {
+            try
+            {
+                JobService.ServiceWCFClient svc = new JobService.ServiceWCFClient();
+                Guid[] jobNames = svc.selectExpressionNames();
+                return Array.ConvertAll(jobNames, x => x.ToString());
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        //job difficulty (used in X)
+        public double[] selectExpressionDifficulty()
+        {
+            try
+            {
+                JobService.ServiceWCFClient svc = new JobService.ServiceWCFClient();
+                return svc.selectExpressionDifficulty();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
