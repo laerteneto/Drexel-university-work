ac13874bf6ca159c9b034bf94a415ba794b18cfc
laerteneto <laerte-m@hotmail.com>
2016-08-19 15:53:18 -0400

    add

---
+++ Top10JobsAnalysis/Top10JobsAnalysis/JobData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Top10JobsAnalysis
+{
+    class JobData
+    {
+        String job_name;
+        String job_ID;
+        String job_description;
+        private int[] job_metrics;
+
+        public JobData(String name, String ID, String description, int[] metrics)
+        {
+            job_name = name;
+            job_ID = ID;
+            job_description = description;
+            job_metrics = metrics;
+        }
+
+        public int[] getMetrics() { return job_metrics; }
+        public String getName() { return job_name;  }
+        public String getDescription() { return job_description; }
+        public String getID() { return job_ID; }
+    }
+}
---
+++ Top10JobsAnalysis/Top10JobsAnalysis/UserPrediction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Top10JobsAnalysis
+{
+    class UserPrediction
+    {
+        String user_name;
+        String[] jobs_IDs;
+
+        public UserPrediction(String name, String[] IDs)
+        {
+            user_name = name;
+            jobs_IDs = IDs;
+        }
+
+        public String getName() { return user_name; }
+        public String[] getJobsIDs() { return jobs_IDs; }
+    }
+}
